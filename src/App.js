388cf6311e899cfc5b9c65e291c2f9ab6d45a416
AbdAlfadeel-Hamdy <abdalfadeelh@gmail.com>
2022-09-23 20:22:56 +0200

    initial-setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import { Switch, Route, Redirect } from "react-router-dom";
+import NewQuote from "./pages/NewQuote";
+import QuoteDetail from "./pages/QuoteDetail";
+import AllQuotes from "./pages/AllQuotes";
+
 function App() {
   return (
     <div>
-
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to="/quotes" />
+        </Route>
+        <Route path="/quotes" exact>
+          <AllQuotes />
+        </Route>
+        <Route path="/quotes/:quoteId">
+          <QuoteDetail />
+        </Route>
+        <Route path="/new-quote">
+          <NewQuote />
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/pages/QuoteDetail.js
@@ -0,0 +1,16 @@
+import { Route, useParams } from "react-router-dom";
+import Comments from "../components/comments/Comments";
+const QuoteDetail = () => {
+  const params = useParams();
+  return (
+    <section>
+      <h1>QuoteDetail</h1>
+      <p>{params.quoteId}</p>
+      <Route path={`/quotes/${params.quoteId}/comments`}>
+        <Comments />
+      </Route>
+    </section>
+  );
+};
+
+export default QuoteDetail;
