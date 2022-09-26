26b306383135873ebd5a54f239c280690e9f0938
AbdAlfadeel-Hamdy <abdalfadeelh@gmail.com>
2022-09-26 20:09:05 +0200

    use Firebase

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,12 +1,21 @@
 import QuoteForm from "../components/quotes/QuoteForm";
 import { useHistory } from "react-router-dom";
+import useHttp from "../hooks/use-http";
+import { useEffect } from "react";
+import { addQuote } from "../lib/api";
 const NewQuote = () => {
   const history = useHistory();
-  const addQuoteHandler = (quoteData) => {
-    console.log(quoteData);
+  const { sendRequest, status } = useHttp(addQuote);
+  useEffect(() => {
+    if (status !== "completed") return;
     history.push("/quotes");
+  }, [history, status]);
+  const addQuoteHandler = (quoteData) => {
+    sendRequest(quoteData);
   };
-  return <QuoteForm onAddQuote={addQuoteHandler} />;
+  return (
+    <QuoteForm isLoading={status === "pending"} onAddQuote={addQuoteHandler} />
+  );
 };
 
 export default NewQuote;
--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,4 +1,4 @@
-import { Fragment } from "react";
+import { Fragment, useEffect } from "react";
 import {
   Link,
   Route,
@@ -8,16 +8,33 @@ import {
 } from "react-router-dom";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
 import Comments from "../components/comments/Comments";
-import { DUMMY_QUOTES } from "./AllQuotes";
+import useHttp from "../hooks/use-http";
+import { getSingleQuote } from "../lib/api";
+import LoadingSpinner from "../components/UI/LoadingSpinner";
 const QuoteDetail = () => {
   const params = useParams();
+  const { quoteId } = params;
   const match = useRouteMatch();
-  console.log(match);
-  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
-  if (!quote) return <p>Quote not found!</p>;
+  const {
+    sendRequest,
+    status,
+    error,
+    data: loadedQuote,
+  } = useHttp(getSingleQuote, true);
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]);
+  if (status === "pending")
+    return (
+      <div className="centered">
+        <LoadingSpinner />
+      </div>
+    );
+  if (error) return <p className="centered">{error}</p>;
+  if (!loadedQuote) return <p>Quote not found!</p>;
   return (
     <Fragment>
-      <HighlightedQuote text={quote.text} author={quote.author} />
+      <HighlightedQuote text={loadedQuote.text} author={loadedQuote.author} />
       <Switch>
         <Route path={match.path} exact>
           <div className="centered">
